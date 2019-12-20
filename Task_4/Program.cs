4522263455fc3d33014adecd300c641501cdcbe0
GalinaAdolshina <halyna.adolshyna@teaminternational.com>
2019-12-20 17:35:30 +0200

    start task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Enter the word");
+            string word = Console.ReadLine();
+          
+            Console.ReadKey();
+        }
+
+     
+    }
+}
