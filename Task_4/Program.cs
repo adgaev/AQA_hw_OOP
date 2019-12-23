eac4c89dffad4967aa66f8fe6aaab69fbe93a1ab
GalinaAdolshina <halyna.adolshyna@teaminternational.com>
2019-12-23 18:37:23 +0200

    code clean up

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,42 +1,34 @@
 ﻿using System;
 
-
 namespace Task_4
 {
     public class Program
     {
-
         public static void Main()
         {
             int i = 0;
             do
             {
                 Console.Write("Enter word: ");
-
                 string word = Console.ReadLine();
-
                 char[] mirror = word.ToCharArray();
-
 
                 if (!Array.TrueForAll(mirror, char.IsLetter))
                 {
                     Console.WriteLine("Wrong format. Only alphabet is supported!\n");
-
                 }
 
                 else
-
                 {
                     Array.Reverse(mirror);
                     string palindrome = new string(mirror);
-
-                    Console.WriteLine("Palindrome? " + word.Equals(palindrome)+"\n");
+                    Console.WriteLine("Palindrome? " + word.Equals(palindrome) + "\n");
                 }
 
                 i++;
             }
-            while (i < 3);
 
+            while (i < 3);
         }
     }
 }
