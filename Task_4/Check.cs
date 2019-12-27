1c8e8fa25b05ba22cd90d6054eec5365822c4ab9
GalinaAdolshina <halyna.adolshyna@teaminternational.com>
2019-12-27 15:58:54 +0200

    added separate class IsEmpty

---
+++ Task_4/Check.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task_4
+{
+    public class Check
+    {
+        public string word;
+        public  string IsEmpty()
+        {
+           
+            int i = 0;
+            do
+            {
+                Console.Write("Enter word: ");
+                string word = Console.ReadLine().ToLower();
+
+                if (String.IsNullOrWhiteSpace(word))
+                {
+                    Console.WriteLine("IsNullOrWhiteSpace.");
+                }
+
+                else
+                {
+                   return  word = word.Replace(" ", String.Empty);
+                }
+
+                i++;
+            }
+            while (String.IsNullOrWhiteSpace(word) && i < 3);
+            return word;
+        }
+
+        public bool IsPalindrome()
+        {
+            word = IsEmpty();
+            
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+
+                if (word[i++] != word[word.Length - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+        public void IsPalindromeResult()
+        {
+          
+            if (IsPalindrome())
+            {
+                Console.WriteLine("Is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Is not a Palindrome!");
+            }
+        }
+    }
+}
--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -5,58 +5,18 @@ namespace Task_4
 {
     public class Program
     {
-        public static void Main()
+
+        public static void Main(string[] args)
         {
-            int i = 0;
-            do
-            {
-                Console.Write("Enter word: ");
-                string word = Console.ReadLine().ToLower();
-                word = word.Replace(" ", String.Empty);
+            Check check = new Check();
+            check.IsPalindromeResult();
 
-                if (String.IsNullOrEmpty(word))
-
-                {
-                    Console.WriteLine("Is Null Or Empty");
-                }
-
-                else
-                {
-                    Console.Write("Palindrome? ");
-
-                    if (IsPalindrome(word))
-                    {
-                        Console.WriteLine("Yes");
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("No");
-                    }
-                    break;
-                }
-                i++;
-            }
-
-            while (i < 3);
             Console.ReadKey();
-        }
-
-
-        public static bool IsPalindrome(string word)
-
-        {
-            for (int i = 0; i < word.Length - 1; i++)
-            {
-                if (word[i++] != word[word.Length - i])
-                {
-                    return false;
-                }
-            }
-
-            return true;
 
         }
+
+
+
 
     }
 }
