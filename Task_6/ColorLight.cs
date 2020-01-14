3457ab3b81ee3c3bed583c0d2e7198e14f8ad289
GalinaAdolshina <halyna.adolshyna@teaminternational.com>
2020-01-14 15:25:21 +0200

    updated

--- Task_6/ColorLight.cs
+++ Task_6/ColorLight.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Task_6
 {
-	public class ColorLight
-	{
-	}
+   public class ColorLight
+    {
+    }
 }
